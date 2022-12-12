6df314a2b9e81eea80118a7cb07152fee2923712
Alastair Zotos <alastairzotos@gmail.com>
2022-12-12 20:12:44 +0700

    initial commit

---
+++ apps/api/src/main.ts
@@ -0,0 +1,17 @@
+import { RequestMethod } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1', {
+    exclude: [{ path: 'health', method: RequestMethod.GET }],
+  });
+
+  app.enableCors();
+
+  await app.listen(3001);
+}
+
+bootstrap();
