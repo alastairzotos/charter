38999f00ab458b45df2a9730be3eb937a839dff1
Alastair Zotos <alastairzotos@gmail.com>
2022-12-19 17:31:34 +0700

    chore: housekeeping

--- apps/web/src/components/breadcrumbs/index.tsx
+++ apps/web/src/components/breadcrumbs/index.tsx
@@ -15,7 +15,13 @@ export const Breadcrumbs: React.FC<Props> = ({ current, list }) => {
     <MuiBreadcrumbs sx={{ mb: 3 }}>
       {
         list.map(item => (
-          <Link key={item.href} href={item.href} underline="hover" color="inherit" component={NextLink}>
+          <Link
+            key={item.href}
+            href={item.href}
+            underline="hover"
+            color="inherit"
+            component={NextLink}
+          >
             {item.title}
           </Link>
         ))
