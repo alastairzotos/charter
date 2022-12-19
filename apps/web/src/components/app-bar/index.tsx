15d0ae9bf2ffae45e2dbb73b7afba454c8301dcf
Alastair Zotos <alastairzotos@gmail.com>
2022-12-19 19:43:23 +0700

    chore: remove toolbar icon from app bar

--- apps/web/src/components/app-bar/index.tsx
+++ apps/web/src/components/app-bar/index.tsx
@@ -4,8 +4,6 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import { useUserState } from '../../state/user';
 import Link from 'next/link';
 import { urls } from '../../urls';
@@ -17,15 +15,6 @@ export const AppBar: React.FC = () => {
     <Box sx={{ flexGrow: 1 }}>
       <MuiAppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             BookingYourBoatTrip
           </Typography>
