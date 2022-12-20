0ec14aaaf5cce4a8e56d23875842360948855854
Alastair Zotos <alastairzotos@gmail.com>
2022-12-20 20:54:06 +0700

    refactor: remove state.ts files

--- apps/web/src/state/trips/index.ts
+++ apps/web/src/state/trips/index.ts
@@ -1,1 +1,107 @@
-export * from './state';
+import { TripDto, TripNoId } from 'dtos';
+import create from 'zustand';
+import { FetchStatus } from '../../models';
+import { TripsService } from '../../services/trips.service';
+
+export interface TripsStateValues {
+  currentOperatorId?: string;
+
+  loadTripsStatus?: FetchStatus;
+  trips: TripDto[];
+
+  loadTripStatus?: FetchStatus;
+  trip?: TripDto;
+
+  updateTripStatus?: FetchStatus;
+  createTripStatus?: FetchStatus;
+  deleteTripStatus?: FetchStatus;
+}
+
+export interface TripsStateActions {
+  loadTripsForOperator: (operatorId: string) => Promise<void>;
+  loadTrip: (id: string) => Promise<void>;
+  updateTrip: (id: string, newTrip: Partial<TripNoId>) => Promise<void>;
+  deleteTrip: (id: string) => Promise<void>;
+  createTrip: (trip: TripNoId) => Promise<void>;
+}
+
+export type TripsState = TripsStateValues & TripsStateActions;
+
+export const createTripsState = (initialValues: TripsStateValues, tripsService: Pick<TripsService, keyof TripsService>) =>
+  create<TripsState>((set, self) => ({
+    ...initialValues,
+
+    loadTripsForOperator: async (operatorId) => {
+      set({ currentOperatorId: operatorId });
+
+      try {
+        set({ loadTripsStatus: 'fetching' });
+
+        const trips = await tripsService.getTripsForOperator(operatorId);
+
+        set({ loadTripsStatus: 'success', trips });
+      } catch {
+        set({ loadTripsStatus: 'error' });
+      }
+    },
+
+    loadTrip: async (id) => {
+      try {
+        set({ loadTripStatus: 'fetching' });
+        
+        const trip = await tripsService.getTrip(id);
+
+        set({ loadTripStatus: 'success', trip });
+      } catch {
+        set({ loadTripStatus: 'error' });
+      }
+    },
+
+    updateTrip: async (id, newTrip) => {
+      try {
+        set({ updateTripStatus: 'fetching' });
+
+        await tripsService.updateTrip(id, newTrip);
+
+        set({ updateTripStatus: 'success' });
+
+        self().loadTripsForOperator(self().currentOperatorId!);
+      } catch {
+        set({ updateTripStatus: 'error' });
+      }
+    },
+
+    deleteTrip: async (id) => {
+      try {
+        set({ deleteTripStatus: 'fetching' });
+
+        await tripsService.deleteTrip(id);
+
+        set({ deleteTripStatus: 'success' });
+
+        self().loadTripsForOperator(self().currentOperatorId!);
+      } catch {
+        set({ deleteTripStatus: 'error' });
+      }
+    },
+
+    createTrip: async (trip) => {
+      try {
+        set({ createTripStatus: 'fetching' });
+
+        await tripsService.createTrip(trip);
+
+        set({ createTripStatus: 'success' });
+        self().loadTripsForOperator(self().currentOperatorId!);
+      } catch {
+        set({ createTripStatus: 'error' });
+      }
+    }
+  }))
+
+export const useTripsState = createTripsState(
+  {
+    trips: []
+  },
+  new TripsService()
+)
--- apps/web/src/state/user/index.ts
+++ apps/web/src/state/user/index.ts
@@ -1,1 +1,97 @@
-export * from './state';
+import { UserDetails } from 'dtos';
+import create from 'zustand';
+import * as jwt from 'jsonwebtoken';
+
+import { FetchStatus } from '../../models';
+import { UserService } from '../../services/user.service';
+import { LocalStorageService } from '../../services/localstorage.service';
+
+const ACCESS_TOKEN_LOCALSTORAGE_KEY = 'boatrental:auth-token';
+
+export interface UserStateValues {
+  initialised: boolean;
+  registerStatus?: FetchStatus;
+  loginStatus?: FetchStatus;
+  accessToken?: string;
+  loggedInUser?: UserDetails;
+}
+
+export interface UserStateActions {
+  initLocalStorage: () => void;
+
+  registerUser: (givenName: string, email: string, password: string) => Promise<void>;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => void;
+}
+
+export type UserState = UserStateValues & UserStateActions;
+
+export const createUserState = (
+  initialState: UserStateValues,
+  userService: Pick<UserService, keyof UserService>,
+  localStorage: Pick<LocalStorageService, keyof LocalStorageService>,
+) =>
+  create<UserState>((set, self) => ({
+    ...initialState,
+
+    initLocalStorage: () => {
+      const accessToken = localStorage.get(ACCESS_TOKEN_LOCALSTORAGE_KEY);
+      if (accessToken) {
+        set({
+          initialised: true,
+          accessToken,
+          loggedInUser: jwt.decode(accessToken) as UserDetails,
+        });
+      } else {
+        set({ initialised: true });
+      }
+    },
+
+    registerUser: async (givenName, email, password) => {
+      try {
+        set({ registerStatus: 'fetching' });
+
+        await userService.registerUser(givenName, email, password);
+
+        set({ registerStatus: 'success' });
+      } catch {
+        set({ registerStatus: 'error' });
+      }
+    },
+
+    login: async (email, password) => {
+      try {
+        set({ loginStatus: 'fetching' });
+
+        const { accessToken } = await userService.loginUser(email, password);
+
+        localStorage.set(ACCESS_TOKEN_LOCALSTORAGE_KEY, accessToken);
+        
+        set({
+          loginStatus: 'success',
+          accessToken,
+          loggedInUser: jwt.decode(accessToken) as UserDetails
+        });
+      } catch {
+        set({ loginStatus: 'error' });
+      }
+    },
+
+    logout: () => {
+      set({
+        loginStatus: undefined,
+        accessToken: undefined,
+        loggedInUser: undefined,
+      })
+
+      localStorage.remove(ACCESS_TOKEN_LOCALSTORAGE_KEY);
+    }
+  }))
+
+export const useUserState = createUserState(
+  {
+    initialised: false,
+  },
+  new UserService(),
+  new LocalStorageService(),
+);
