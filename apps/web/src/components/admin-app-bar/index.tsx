e8f458433c424365ed996ef9fa289fa6e99e303d
Alastair Zotos <alastairzotos@gmail.com>
2022-12-23 16:26:01 +0700

    feat: logo

--- apps/web/src/components/admin-app-bar/index.tsx
+++ apps/web/src/components/admin-app-bar/index.tsx
@@ -6,12 +6,17 @@ import Typography from '@mui/material/Typography';
 import { AppBarLoginLogout } from '../app-bar-login-logout';
 import Link from 'next/link';
 import { urls } from '../../urls';
+import Image from 'next/image';
 
 export const AdminAppBar: React.FC = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <MuiAppBar position="static" color="secondary">
         <Toolbar>
+          <Link href={urls.home()}>
+            <Image src="/logo.png" alt="Booking your boat trip logo" width={64} height={64} />
+          </Link>
+
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             <Link href={urls.admin.home()} style={{ textDecoration: 'none', color: 'white' }}>
               BookingYourBoatTrip Admin
--- apps/web/src/components/user-app-bar/index.tsx
+++ apps/web/src/components/user-app-bar/index.tsx
@@ -6,12 +6,17 @@ import Typography from '@mui/material/Typography';
 import { AppBarLoginLogout } from '../app-bar-login-logout';
 import { urls } from '../../urls';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export const UserAppBar: React.FC = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <MuiAppBar position="static">
         <Toolbar>
+          <Link href={urls.home()}>
+            <Image src="/logo.png" alt="Booking your boat trip logo" width={64} height={64} />
+          </Link>
+
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             <Link href={urls.home()} style={{ textDecoration: 'none', color: 'white' }}>
               BookingYourBoatTrip
