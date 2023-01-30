cc5a5abf900f325e0b81032117025d9125f80dd4
Alastair Zotos <alastairzotos@gmail.com>
2023-01-30 17:17:43 +0700

    refactor: formik on trip forms

--- apps/web/src/components/trip-manage/index.tsx
+++ apps/web/src/components/trip-manage/index.tsx
@@ -1,13 +1,18 @@
 import { TripNoId } from 'dtos';
-import React, { useState } from 'react';
-import { Button, CircularProgress, TextField, Typography, Box } from '@mui/material';
+import React from 'react';
+import { Typography } from '@mui/material';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import { FetchStatus } from '../../models';
 import { useRouter } from 'next/router';
 import { urls } from '../../urls';
 import { ImageDropzone } from '../image-dropzone';
-import { DeleteConfirmModal } from '../modals/delete-confirm';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import { TextField } from 'formik-mui';
+import * as yup from 'yup';
+import { Titled } from '../titled';
+import { FormBox } from '../form-box';
+import { SaveAndDelete } from '../save-delete';
 
 interface Props {
   operatorId: string;
@@ -22,147 +27,109 @@ interface Props {
   deleteStatus?: FetchStatus;
 }
 
-export const ManageTripForm: React.FC<Props> = ({ operatorId, title, id, trip, onSave, saveStatus, onDelete, deleteStatus }) => {
+const validationSchema: yup.SchemaOf<Omit<TripNoId, 'operator'>> = yup.object().shape({
+  name: yup.string().required('Name is required'),
+  description: yup.string().required('Description is required'),
+  startTime: yup.string().required('Start time is required'),
+  duration: yup.string().required('Duration is required'),
+  startLocation: yup.string().required('Start location is required'),
+  adultPrice: yup.number().required('Adult price is required').typeError('Price must be a number'),
+  childPrice: yup.number().required('Child price is required').typeError('Price must be a number'),
+  photos: yup.array().of(yup.string().required('Photo is required')),
+})
+
+export const ManageTripForm: React.FC<Props> = ({ operatorId, title, trip, onSave, saveStatus, onDelete, deleteStatus }) => {
   const router = useRouter();
 
-  const [name, setName] = useState(trip.name);
-  const [duration, setDuration] = useState(trip.duration);
-  const [startLocation, setStartLocation] = useState(trip.startLocation);
-  const [startTime, setStartTime] = useState(trip.startTime);
-  const [description, setDescription] = useState(trip.description);
-  const [photos, setPhotos] = useState(trip.photos);
-  const [adultPrice, setAdultPrice] = useState(String(trip.adultPrice ?? 0));
-  const [childPrice, setChildPrice] = useState(String(trip.childPrice ?? 0));
-
-  const [deleteModalOpen, setDeleteModalOpen] = useState(false);
-
-  const isValid = name.trim().length > 0 && duration.trim().length > 0 && startLocation.trim().length > 0 && startTime.trim().length > 0;
-
-  const isFetching = saveStatus === 'fetching' || deleteStatus === 'fetching';
-
-  const handleDeleteTrip = async () => {
+  const handleDeleteTrip = onDelete && (async () => {
     if (!!onDelete) {
       await onDelete();
       router.push(urls.admin.operator(operatorId));
     }
-  }
+  })
 
   return (
-    <Box
-      sx={{
-        p: 3,
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 2
-      }}
+    <Formik
+      initialValues={trip}
+      validationSchema={validationSchema}
+      onSubmit={values => onSave({ ...values, operator: operatorId as any })}
     >
-      <Typography variant="h4">{title}</Typography>
+      {({ isValid, values, setValues }) => (
+        <Titled title={title}>
+          <Form>
+            <FormBox>
+              <Field
+                component={TextField}
+                name="name"
+                label="Trip name"
+              />
 
-      <TextField
-        placeholder='Trip name'
-        value={name}
-        onChange={e => setName(e.target.value)}
-        disabled={isFetching}
-      />
+              <Field
+                component={TextField}
+                name="duration"
+                label="Trip duration"
+              />
 
-      <TextField
-        placeholder='Trip duration'
-        value={duration}
-        onChange={e => setDuration(e.target.value)}
-        disabled={isFetching}
-      />
+              <Field
+                component={TextField}
+                name="startLocation"
+                label="Starting location"
+              />
 
-      <TextField
-        placeholder='Starting location'
-        value={startLocation}
-        onChange={e => setStartLocation(e.target.value)}
-        disabled={isFetching}
-      />
+              <Field
+                component={TextField}
+                name="startTime"
+                label="Start time"
+              />
 
-      <TextField
-        placeholder='Start time'
-        value={startTime}
-        onChange={e => setStartTime(e.target.value)}
-        disabled={isFetching}
-      />
+              <Field
+                component={TextField}
+                name="description"
+                label="Description"
+                multiline
+                rows={4}
+              />
 
-      <TextField
-        placeholder='Description'
-        value={description}
-        onChange={e => setDescription(e.target.value)}
-        disabled={isFetching}
-        multiline
-        rows={4}
-      />
+              <Field
+                component={TextField}
+                name="adultPrice"
+                label="Adult price"
+              />
 
-      <TextField
-        placeholder='Adult Price'
-        value={adultPrice}
-        onChange={e => setAdultPrice(e.target.value)}
-        disabled={isFetching}
-      />
+              <Field
+                component={TextField}
+                name="childPrice"
+                label="Child price"
+              />
 
-      <TextField
-        placeholder='Child Price'
-        value={childPrice}
-        onChange={e => setChildPrice(e.target.value)}
-        disabled={isFetching}
-      />
+              <ImageDropzone
+                multiple
+                onReceiveUrls={photos => setValues({ ...values, photos })}
+              >
+                <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+                  {values.photos.map(photo => (
+                    <ImageListItem key={photo}>
+                      <img src={`${photo}?w=164&h=164&fit=crop&auto=format`} loading="lazy" />
+                    </ImageListItem>
+                  ))}
+                </ImageList>
+              </ImageDropzone>
+              <ErrorMessage name="photos" />
 
-      <Typography variant="h6">Photos</Typography>
-      <ImageDropzone multiple onReceiveUrls={setPhotos}>
-        <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
-          {photos.map(photo => (
-            <ImageListItem key={photo}>
-              <img src={`${photo}?w=164&h=164&fit=crop&auto=format`} loading="lazy" />
-            </ImageListItem>
-          ))}
-        </ImageList>
-      </ImageDropzone>
+              <SaveAndDelete
+                isValid={isValid}
+                saveStatus={saveStatus}
+                onDelete={handleDeleteTrip}
+                deleteStatus={deleteStatus}
+                deleteModalTitle="Delete trip?"
+                deleteModalText="Are you sure you want to delete this trip?"
+              />
 
-      <Box
-        sx={{ display: 'flex', justifyContent: 'space-between' }}
-      >
-        <Button
-          variant="contained"
-          disabled={!isValid || isFetching}
-          sx={{ maxWidth: '20%' }}
-          onClick={() => onSave({
-            name,
-            duration,
-            startLocation,
-            startTime,
-            description,
-            photos,
-            adultPrice: parseFloat(adultPrice),
-            childPrice: parseFloat(childPrice),
-            operator: operatorId as any
-          })}
-        >
-          {saveStatus === 'fetching' ? <CircularProgress size={20} /> : 'Save'}
-        </Button>
-
-        {!!onDelete && (
-          <Button
-            disabled={isFetching}
-            color="warning"
-            onClick={() => setDeleteModalOpen(true)}
-          >
-            Delete
-          </Button>
-        )}
-      </Box>
-
-      {saveStatus === 'error' && <Typography>There was an error saving the trip data</Typography>}
-
-      <DeleteConfirmModal
-        title="Delete trip?"
-        content="Are you sure you want to delete this trip?"
-        open={deleteModalOpen}
-        onClose={() => setDeleteModalOpen(false)}
-        onDelete={handleDeleteTrip}
-        deleteStatus={deleteStatus}
-      />
-    </Box>
+              {saveStatus === 'error' && <Typography>There was an error saving the trip data</Typography>}
+            </FormBox>
+          </Form>
+        </Titled>
+      )}
+    </Formik>
   )
 }
