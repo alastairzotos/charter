0cf0822330f70fa328c1bda57c1c0348e0cc0bdd
Alastair Zotos <alastairzotos@gmail.com>
2023-02-01 17:46:30 +0700

    chore: formatting

--- apps/web/src/components/app-bar-logged-in-button/index.tsx
+++ apps/web/src/components/app-bar-logged-in-button/index.tsx
@@ -3,9 +3,9 @@ import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import { Button, Menu, MenuItem } from "@mui/material";
 import Link from "next/link";
 import React, { useState } from "react";
+import { urls } from "urls";
 
 import { useUserState } from "src/state/user";
-import { urls } from "urls";
 
 export const AppBarLoggedInButton: React.FC = () => {
   const [logout, loggedInUser] = useUserState((s) => [
@@ -41,15 +41,22 @@ export const AppBarLoggedInButton: React.FC = () => {
       </Button>
 
       <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
-        {loggedInUser.role === 'admin' && (
+        {loggedInUser.role === "admin" && (
           <MenuItem>
-            <Link href={urls.admin.home()} style={{ textDecoration: 'none' }}>Admin</Link>
-          </MenuItem>  
+            <Link href={urls.admin.home()} style={{ textDecoration: "none" }}>
+              Admin
+            </Link>
+          </MenuItem>
         )}
-        {loggedInUser.role === 'operator' && (
+        {loggedInUser.role === "operator" && (
           <MenuItem>
-            <Link href={urls.operators.home()} style={{ textDecoration: 'none' }}>Bookings</Link>
-          </MenuItem>  
+            <Link
+              href={urls.operators.home()}
+              style={{ textDecoration: "none" }}
+            >
+              Bookings
+            </Link>
+          </MenuItem>
         )}
         <MenuItem onClick={handleLogoutClick}>Logout</MenuItem>
       </Menu>
