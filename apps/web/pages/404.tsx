956a3628de96fde2198036a0c4d9eaf4f23a775d
Alastair Zotos <alastairzotos@gmail.com>
2023-02-01 16:39:55 +0700

    chore: formatting

--- apps/web/pages/404.tsx
+++ apps/web/pages/404.tsx
@@ -1,8 +1,9 @@
 import { Button, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import Link from "next/link";
-import { SeoHead } from "src/components/seo/head";
 import { urls } from "urls";
+
+import { SeoHead } from "src/components/seo/head";
 
 export default function Page404() {
   return (
@@ -12,12 +13,16 @@ export default function Page404() {
         description="The page you're looking for can't be found"
       />
 
-      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <Typography variant="h3" sx={{ mb: 5 }}>Oops! Looks like you&apos;re lost</Typography>
-        <Link href={urls.home()} style={{ textDecoration: 'none' }}>
+      <Box
+        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      >
+        <Typography variant="h3" sx={{ mb: 5 }}>
+          Oops! Looks like you&apos;re lost
+        </Typography>
+        <Link href={urls.home()} style={{ textDecoration: "none" }}>
           <Button>Return home</Button>
         </Link>
       </Box>
     </div>
-  )
+  );
 }
