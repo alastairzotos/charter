614945f59be3652ea1ff9810396b57cc73ae034a
Alastair Zotos <alastairzotos@gmail.com>
2023-02-09 18:27:51 +0700

    chore: formatting

--- apps/web/src/util/env.ts
+++ apps/web/src/util/env.ts
@@ -1,7 +1,8 @@
-import getConfig from 'next/config';
+import getConfig from "next/config";
 
 export const getEnv = () => ({
   appUrl: getConfig().publicRuntimeConfig.NEXT_PUBLIC_APP_URL as string,
   apiUrl: getConfig().publicRuntimeConfig.NEXT_PUBLIC_API_URL as string,
-  stripePublishableKey: getConfig().publicRuntimeConfig.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string,
+  stripePublishableKey: getConfig().publicRuntimeConfig
+    .NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string,
 });
