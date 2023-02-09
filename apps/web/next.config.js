7c434603fa111ba9167654fdb9f7d0ffb82e3168
Alastair Zotos <alastairzotos@gmail.com>
2023-02-09 18:27:27 +0700

    fix: nextjs env args

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -9,4 +9,9 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  }
 };
--- apps/web/src/util/env.ts
+++ apps/web/src/util/env.ts
@@ -1,6 +1,7 @@
+import getConfig from 'next/config';
+
 export const getEnv = () => ({
-  appUrl: process.env.NEXT_PUBLIC_APP_URL as string,
-  apiUrl: process.env.NEXT_PUBLIC_API_URL as string,
-  stripePublishableKey: process.env
-    .NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string,
+  appUrl: getConfig().publicRuntimeConfig.NEXT_PUBLIC_APP_URL as string,
+  apiUrl: getConfig().publicRuntimeConfig.NEXT_PUBLIC_API_URL as string,
+  stripePublishableKey: getConfig().publicRuntimeConfig.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string,
 });
