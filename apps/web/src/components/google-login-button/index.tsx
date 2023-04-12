3a91f7cf811de1742313bc30991314db72923a36
Alastair Zotos <alastairzotos@gmail.com>
2023-04-12 15:28:20 +0800

    fix: google login button styling

--- apps/web/src/components/google-login-button/index.tsx
+++ apps/web/src/components/google-login-button/index.tsx
@@ -1,13 +1,33 @@
-import { Button } from "@mui/material";
+import { Button, styled } from "@mui/material";
 import React from "react";
 
-import styles from "src/components/google-login-button/google-login-button.module.css";
+const MainButton = styled(Button)(() => ({
+  height: 40,
+  fontSize: "1.1em",
+  fontWeight: 500,
+  textTransform: "uppercase",
+  backgroundColor: "#345eeb",
+  borderRadius: 0,
+
+  "&:hover": {
+    backgroundColor: "#2241a8",
+  },
+
+  "& i": {
+    color: "white",
+  },
+
+  "& span": {
+    color: "white",
+    marginLeft: 8,
+  },
+}));
 
 export const GoogleLoginButton: React.FC<
   React.ComponentProps<typeof Button>
 > = (props) => (
-  <Button {...props} size="large" className={styles.button}>
+  <MainButton {...props} size="large">
     <i className="fa fa-google"></i>
-    <span className={styles.span}>Login with Google</span>
-  </Button>
+    <span>Login with Google</span>
+  </MainButton>
 );
