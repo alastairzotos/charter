e92b0b1fbe97e4305d224ddd787f0b8ae4c18067
Alastair Zotos <alastairzotos@gmail.com>
2023-04-25 17:16:37 +0800

    build: nicer schema fields table ui

--- apps/web/src/components/service-schema-fields-selector/index.tsx
+++ apps/web/src/components/service-schema-fields-selector/index.tsx
@@ -1,8 +1,9 @@
-import { Box, Button } from "@mui/material";
+import { Button } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
 import { ServiceSchemaFieldDto } from "dtos";
 import React from "react";
 
-import { ServiceSchemaField } from "components/service-schema-field";
+import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
 
 interface Props {
   fields: ServiceSchemaFieldDto[];
@@ -14,39 +15,92 @@ export const ServiceSchemaFieldsSelector: React.FC<Props> = ({
   onChange,
 }) => {
   return (
-    <Box sx={{ p: 3 }}>
-      {fields.map((field, index) => (
-        <ServiceSchemaField
-          key={index}
-          field={field}
-          onChange={(newField) =>
-            onChange(
-              fields.map((field, newFieldIndex) =>
-                newFieldIndex === index ? newField : field
-              )
-            )
-          }
-          onDelete={() => onChange(fields.filter((_, i) => i !== index))}
-        />
-      ))}
-
-      <div />
-
-      <Button
-        variant="outlined"
-        onClick={() =>
-          onChange([
-            ...fields,
-            {
-              key: "key",
-              label: "New field",
-              type: "string",
+    <>
+      <DataGrid
+        columns={[
+          {
+            width: 200,
+            field: "key",
+            headerName: "Key",
+            editable: true,
+          },
+          {
+            width: 250,
+            field: "label",
+            headerName: "Label",
+            editable: true,
+          },
+          {
+            width: 150,
+            field: "type",
+            headerName: "Field type",
+            type: "singleSelect",
+            editable: true,
+            valueOptions: [
+              {
+                value: "string",
+                label: "String",
+              },
+              {
+                value: "multiline-text",
+                label: "Multiline Text",
+              },
+              {
+                value: "time",
+                label: "Time",
+              },
+              {
+                value: "timeframe",
+                label: "Timeframe",
+              },
+            ],
+          },
+          {
+            field: "delete",
+            headerName: "Actions",
+            type: "actions",
+            getActions: (params) => {
+              return [
+                <GridActionsCellItem
+                  icon={<CloseIcon />}
+                  label="Delete"
+                  onClick={() =>
+                    onChange(fields.filter((field) => field.key !== params.id))
+                  }
+                />,
+              ];
             },
-          ])
-        }
-      >
-        Add field
-      </Button>
-    </Box>
+          },
+        ]}
+        rows={fields}
+        getRowId={(row) => row.key}
+        processRowUpdate={(newRow) => {
+          onChange(
+            fields.map((field) => (field.key === newRow.key ? newRow : field))
+          );
+
+          return newRow;
+        }}
+        hideFooter
+      />
+
+      <div>
+        <Button
+          variant="outlined"
+          onClick={() =>
+            onChange([
+              ...fields,
+              {
+                key: "key",
+                label: "New field",
+                type: "string",
+              },
+            ])
+          }
+        >
+          Add field
+        </Button>
+      </div>
+    </>
   );
 };
