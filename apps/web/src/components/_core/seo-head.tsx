7084fc310b9df570aaf6e2415037a04bf00634e3
Alastair Zotos <alastairzotos@gmail.com>
2023-05-02 14:57:50 +0800

    build: better opengraph tags

--- apps/web/src/components/_core/seo-head.tsx
+++ apps/web/src/components/_core/seo-head.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import React from "react";
+import { getEnv } from "util/env";
 
 import { APP_NAME } from "util/misc";
 
@@ -10,15 +12,21 @@ interface Props {
 
 export const SeoHead: React.FC<Props> = ({ subtitle, description }) => {
   const title = `${APP_NAME} ${subtitle ? ` | ${subtitle}` : ""}`;
+  const router = useRouter();
+  const canonical = `${getEnv().appUrl}${router.asPath}`;
 
   return (
     <Head>
       <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="canonical" href={canonical} />
+
       <title>{title}</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="description" content={description} />
+
       <meta name="og:title" content={title} />
       <meta name="og:description" content={description} />
+      <meta name="og:url" content={canonical} />
     </Head>
   );
 };
