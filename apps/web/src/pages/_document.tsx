47dd38a78be397f5d6647223b80eaa4eeb99fe50
Alastair Zotos <alastairzotos@gmail.com>
2023-05-18 16:15:10 +0800

    feat: disable ga in dev

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -3,23 +3,28 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+      {!!process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID ? (
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
   
               gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');`,
-          }}
-        />
-      </Head>
+            }}
+          />
+        </Head>
+      ) : (
+        <Head />
+      )}
+
       <body>
         <Main />
         <NextScript />
