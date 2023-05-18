da38b84ab6c3a3303f52ec0bef38e289b6cdc02b
Alastair Zotos <alastairzotos@gmail.com>
2023-05-18 21:19:20 +0800

    feat: nicer service create button

--- apps/manager/src/components/operator/dashboard/services/service-create-input.tsx
+++ apps/manager/src/components/operator/dashboard/services/service-create-input.tsx
@@ -1,8 +1,9 @@
-import InsertPhotoIcon from "@mui/icons-material/InsertPhoto";
-import { Box, TextField, Typography, Autocomplete } from "@mui/material";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { Typography, Menu, MenuItem, Button } from "@mui/material";
 import { ServiceSchemaDto } from "dtos";
+import { NestedMenuItem } from "mui-nested-menu";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { StatusSwitch } from "ui";
 
 import { useOperatorDashboard } from "contexts/operator-dashboard";
@@ -19,13 +20,27 @@ export const ServiceCreateInput: React.FC<Props> = ({ operatorId }) => {
   const [loadServiceSchemasStatus, loadServiceSchemas, serviceSchemas] =
     useLoadServiceSchemas((s) => [s.status, s.request, s.value]);
 
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+
   useEffect(() => {
     loadServiceSchemas();
   }, []);
 
   const handleServiceSchemaClick = (schema: ServiceSchemaDto) => {
+    setAnchorEl(null);
     router.push(getServiceCreateUrl(operatorId, schema._id));
   };
+
+  const serviceTypesMenu = serviceSchemas?.reduce(
+    (acc, cur) => ({
+      ...acc,
+      [cur.schemaCategory!.name]: {
+        category: cur.schemaCategory!,
+        schemas: [...(acc?.[cur.schemaCategory!.name]?.schemas || []), cur],
+      },
+    }),
+    {} as Record<string, { schemas: ServiceSchemaDto[] }>
+  );
 
   return (
     <StatusSwitch
@@ -34,36 +49,29 @@ export const ServiceCreateInput: React.FC<Props> = ({ operatorId }) => {
         <Typography>There was an error loading the service schemas</Typography>
       }
     >
-      <Autocomplete
-        sx={{ width: 300 }}
-        freeSolo
-        options={serviceSchemas || []}
-        getOptionLabel={(option) => (option as ServiceSchemaDto).name}
-        onChange={(_, v) => handleServiceSchemaClick(v as ServiceSchemaDto)}
-        renderOption={(props, option) => (
-          <Box
-            component="li"
-            sx={{
-              "& > img, svg": { mr: 2, flexShrink: 0, width: 20, height: 20 },
-            }}
-            {...props}
-          >
-            {option.schemaCategory?.photo ? (
-              <img
-                loading="lazy"
-                src={option.schemaCategory?.photo}
-                alt={option.name}
-              />
-            ) : (
-              <InsertPhotoIcon />
-            )}
-            {option.name}
-          </Box>
-        )}
-        renderInput={(params) => (
-          <TextField {...params} label="Add new service" />
-        )}
-      />
+      <Button variant="contained" onClick={(e) => setAnchorEl(e.currentTarget)}>
+        New service <KeyboardArrowDownIcon />
+      </Button>
+
+      <Menu
+        open={!!anchorEl}
+        anchorEl={anchorEl}
+        onClose={() => setAnchorEl(null)}
+      >
+        {serviceTypesMenu &&
+          Object.keys(serviceTypesMenu).map((category) => (
+            <NestedMenuItem key={category} parentMenuOpen label={category}>
+              {serviceTypesMenu[category].schemas.map((schema) => (
+                <MenuItem
+                  key={schema._id}
+                  onClick={() => handleServiceSchemaClick(schema)}
+                >
+                  {schema.name}
+                </MenuItem>
+              ))}
+            </NestedMenuItem>
+          ))}
+      </Menu>
     </StatusSwitch>
   );
 };
